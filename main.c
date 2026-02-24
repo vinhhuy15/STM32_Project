3b1f8a9c49d625d18ba64244d91619c6ffaa5c47
giang vĩnh huy <giangvinhhuy1029@gmail.com>
2026-02-24 10:06:41 +0700

    thêm hàm khởi tạo đọc thẻ rfid

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 // Code khởi tạo STM32F103
 int main() {
     SystemClock_Config();
+    RFID_Init(); // Vừa code thêm dòng này
     // TODO: Cấu hình ngoại vi ở đây
     while(1) {
         // Vòng lặp chính

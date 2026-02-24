ab4a9417fae323466bb9d300468b29436e490ddc
giang vĩnh huy <giangvinhhuy1029@gmail.com>
2026-02-24 10:12:09 +0700

    Cấu hình SPI tốc độ cao cho SD Card

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 // Code khởi tạo STM32F103
 int main() {
     SystemClock_Config();
+    SPI_Init(Fast_Mode); // Thêm dòng này ở nhánh SD Card
     RFID_Init(); // Vừa code thêm dòng này
     // TODO: Cấu hình ngoại vi ở đây
     while(1) {

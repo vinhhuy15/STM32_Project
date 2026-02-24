91c1870f58fa1eccd3cd624454f9dfc0adbabe73
giang vĩnh huy <giangvinhhuy1029@gmail.com>
2026-02-24 10:12:52 +0700

    Cấu hình SPI tốc độ chậm cho an toàn

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 // Code khởi tạo STM32F103
 int main() {
     SystemClock_Config();
+    SPI_Init(Slow_Mode); // Thêm dòng này ở nhánh main
     RFID_Init(); // Vừa code thêm dòng này
     // TODO: Cấu hình ngoại vi ở đây
     while(1) {
